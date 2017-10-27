c6f8496daa85ccb3d6223bb2c5e396ac7cd050a4
LemurPwned <jakub.mojsiejuk@gmail.com>
2017-10-27 11:54:39 +0200

    cleaning

--- run.py
+++
@@ -1,9 +0,0 @@
-from data_assembly import Assembly
-
-if __name__ == "__main__":
-    directory = "data\\0200nm"
-    assembler = Assembly(directory)
-    omf_files, odt_file = assembler.read_simulation_files()
-    data, base_data = assembler.process_simulation_files(omf_files=omf_files,
-                                                            filetype='binary')
-    
