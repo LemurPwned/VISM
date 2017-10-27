2eed8016850cf84f0a5b4c70462f7275930f0a1b
LemurPwned <jakub.mojsiejuk@gmail.com>
2017-10-27 12:27:29 +0200

    added .odt file

--- Parser.py
+++ Parser.py
@@ -2,4 +2,48 @@
 import numpy
 class Parser:
     def __init__(folder):
-        self.folder = folder
+        self.working_folder = folder
+
+    @staticmethod
+    def readFolder(directory):
+        pass
+
+    def getOdtData(filename):
+        if not filename.endswith(".odt"):
+            print("Wrong file type passed, only .odt")
+            return
+        else:
+            header_lines = 4
+            header = []
+            i = 0
+            with open(filename, 'r') as f:
+                while i < header_lines:
+                    lines = f.readline()
+                    header.append(lines)
+                    i += 1
+                units = f.readline()
+                lines = f.readlines()
+            f.close()
+            cols = header[-1]
+            cols = cols.replace("} ", "")
+            cols = cols.replace("{", "")
+            cols = cols.split("Oxs_")
+            del cols[0]
+            cols = [x.strip() for x in cols]
+            dataset = []
+            lines = [x.strip() for x in lines]
+            print("{} lines have been read ".format(len(lines)))
+            lines = [x.split(' ') for x in lines]
+            for line in lines:
+                temp_line = []
+                for el in line:
+                    try:
+                        new_el = float(el)
+                        temp_line.append(new_el)
+                    except:
+                        pass
+                dataset.append(temp_line)
+            dataset = dataset[:-1]
+            df = pd.DataFrame.from_records(dataset, columns=cols)
+            stages = len(lines) -1
+            return df, stages
--- Readme.md
+++ Readme.md
@@ -2,12 +2,25 @@
 
 
 ### Function
-1. readFolder(directory)
+1. readFolder(directory, multipleOmfHeaders=False)
+  -> if multipleOmfHeaders = False then getOmfHeader is returned only for
+    one .omf, else returns for each filename in folder
   -> detects filetype (binary/text)
   -> detects file format (.omf, .odt)
   -> processes dataset
   -> returns omfHeader, odtData (for 2d plots), rawVectorData(from .omf)
 
+2. getOdtData(filename)
+   -> returns dataFrame with columns as specified in .odt filename
+3. getOmfHeader(filename)
+  -> returns .omf header in form of a dictionary
+
 2. drawPlot(odtData, canvas, external_iteration=iterator)
+  -> odtData should be a Series of a dataFrame odtData e.g.
+      odtData contins colums A, B, C of which we want to plot A & B on one plots
+      then
+      drawPlot(odtData[['A', 'B']], cavnas=None, external_iteration=None,
+      update_function=None)
+
   ->draws plots dynamically on provided canvas
   -> can have external iterating function
