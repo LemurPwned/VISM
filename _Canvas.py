46672ed4e6838db1f4acbe74b3caed04654248cf
LemurPwned <jakub.mojsiejuk@gmail.com>
2017-11-03 23:56:10 +0100

    update on test suite

--- _Canvas.py
+++ _Canvas.py
@@ -6,22 +6,33 @@ def _shareData():
     odt_data = Parser.getOdtData(test_odt)
     print("DETECTED COLUMNS:")
     print(odt_data[0].columns)
+    picked_column = 'MR::Energy'
 
     #create data dict that is then passed to canvas
     #this is the exemplary dict, the order must be preserved
     data_dict = {
                 'i': 0,
                 'iterations': odt_data[1],
-                'graph_data': odt_data[0]
+                'graph_data': odt_data[0][picked_column].tolist(),
+                'title' : picked_column
                 }
     new_canvas = Canvas()
     new_canvas.shareData(**data_dict)
-    _runCanvas()
+    new_canvas.check_instance()
+    _runCanvas(new_canvas, odt_data[1])
 
-def _runCanvas():
-    new_canvas = Canvas()
-    new_canvas.createPlotCanvas()
-    new_canvas.replot()
+def _runCanvas(canvas, iterations):
+    canvas.createPlotCanvas()
+    print("DONE CREATING CANVAS... PREPARING OT RUN...")
+    #canvas.runCanvas()
+    _iterateCanvas(canvas, iterations)
+
+def _iterateCanvas(canvas, iterations):
+    while(iterations):
+        canvas.replot()
+        print(canvas.i)
+        canvas.increaseIterator()
+        iterations -= 1
 
 if __name__ == "__main__":
     _shareData()
