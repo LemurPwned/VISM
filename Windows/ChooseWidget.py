d087573a79353ab52cadcb824786d0db57f1fce7
pawelkulig1 <pawelkulig1@gmail.com>
2017-11-11 20:14:25 +0100

    commit before new ideas

--- Windows/ChooseWidget.py
+++ Windows/ChooseWidget.py
@@ -22,7 +22,7 @@ class ChooseWidget(QtWidgets.QWidget):
             self.handler([self.number, "OpenGL"])
         elif self.list.currentItem().text() == "2D plot Widget":
             self.handler([self.number, "2dPlot"])
-        elif self.list.currentItem.text() == "2D layer plot Widget":
+        elif self.list.currentItem().text() == "2D layer plot Widget":
             self.handler([self.number, "2dLayer"])
 
         self.close()
