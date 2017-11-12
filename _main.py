6c966fc99f2116abbe7d80709249633ee6284267
pawelkulig1 <pawelkulig1@gmail.com>
2017-11-12 18:23:40 +0100

    tests updated

--- _main.py
+++ _main.py
@@ -5,11 +5,17 @@ from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore
 from main import MainWindow
+from Parser import Parser
 
 class _MainTester(unittest.TestCase):
 
     def initialize(self):
         self.mainGui = MainWindow()
+
+    def initialize_data(self):
+        self.initialize()
+        test_folder = "data/firstData"
+        self.rawVectorData, self.omf_header, self.odtData, self.stages = Parser.readFolder(test_folder)
 
     def test_initialGui(self):
         self.initialize()
@@ -22,10 +28,10 @@ class _MainTester(unittest.TestCase):
         self.assertFalse(self.mainGui.panes[2].isVisible())
         self.assertFalse(self.mainGui.panes[3].isVisible())
 
-    def test_loadDirectory(self):
+    '''def test_loadDirectory(self):
         self.initialize()
         self.mainGui.actionLoad_Directory.trigger()
-        self.assertTrue(len(self.mainGui.odt_data) > 0)
+        self.assertTrue(len(self.mainGui.odt_data) > 0)'''
 
     def test_ViewListeners(self):
         self.initialize()
@@ -59,7 +65,11 @@ class _MainTester(unittest.TestCase):
 
         #self.mainGui.action2_Windows_Grid.trigger()
     def test_PlotSettings(self):
-        pass
+        self.initialize_data()
+        self.mainGui.actionPlot.trigger()
+        self.mainGui.plotSettingsWindow.accept.click()
+
+        #QTest.mouseClick(self.window.browseButton_2, QtCore.Qt.LeftButton)
         #self.initialize()
 
         #self.
