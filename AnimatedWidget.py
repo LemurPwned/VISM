3572a390c74ba972f2d441eb417c60acb42fcb41
LemurPwned <jakub.mojsiejuk@gmail.com>
2017-11-18 12:30:43 +0100

    abstract widget in animated widget

---
+++ AnimatedWidget.py
@@ -0,0 +1,42 @@
+from abc import abstractmethod, ABC
+
+class AnimatedWidget(ABC):
+    def __init__(self):
+        self._MINIMUM_PARAMS_ = ['i', 'iterations']
+
+    def shareData(self, **kwargs):
+        """
+        @param **kwargs are the arguments to be passed to the main plot iterator
+        """
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def loop_guard(self):
+        self.i %= self.iterations
+
+    @abstractmethod
+    def parameter_check(self):
+        """
+        parameter_check function should define minimum number and list
+        of parameters used by widgets. This function provides a proper
+        way to verify if widget has received every chunk of data it
+        needs in order to be operational
+        """
+        _MISSING_FLAG_ = False
+        for attribute in self._MINIMUM_PARAMS_:
+            try:
+                getattr(self, attribute)
+            except AttributeError:
+                _MISSING_FLAG_ = True
+                msg = "There is an attribute missing: {}".format(attribute)
+                print(msg)
+        return _MISSING_FLAG_
+
+    @abstractmethod
+    def set_i(self, value):
+        """
+        This is iterating setter. Function set_i should provide
+        a neat way to change the current data of Widget and then
+        refresh it immediately.
+        """
+        pass
