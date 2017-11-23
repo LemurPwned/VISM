4e10621d82830020007e2a2dcf8dc3a26a7ad57f
LemurPwned <jakub.mojsiejuk@gmail.com>
2017-11-23 12:08:17 +0100

    idea for faster backend plotting

--- Canvas.py
+++ Canvas.py
@@ -15,16 +15,16 @@ class Canvas(AbstractCanvas):
             raise ValueError(msg)
 
         p_dict = {
-                                'color': 'green',
-                                'line_style': 'dashed',
-                                'marker': '*',
-                                'marker_color': 'blue',
-                                'marker_size': 3
-                                                }
-
+                    'color': 'green',
+                    'line_style': 'dashed',
+                    'marker': '*',
+                    'marker_color': 'blue',
+                    'marker_size': 3
+                    }
         self.canvas_type = 'panel'
         self.fig.suptitle(self.title)
         self.plot_axis = self.fig.add_subplot(111)
+        print(len(self.graph_data))
         self.null_data = [x for x in range(self.iterations)]
         a_handler = self.plot_axis.plot(self.null_data,
             self.graph_data[0:self.i] + self.null_data[self.i:],
@@ -39,6 +39,9 @@ class Canvas(AbstractCanvas):
         self._CANVAS_ALREADY_CREATED_ = True
 
     def replot(self):
-        self.plot_axis.hpl.set_ydata(self.graph_data[0:self.i] + \
-                                self.null_data[self.i:])
+        self.plot_axis.hpl.set_ydata(np.lib.pad(self.graph_data[:self.i],
+                (0, self.iterations-self.i), mode='constant'))
+        print(self.i)
+        print(self.iterations-self.i)
+        print(len(self.graph_data[:self.i]))
         self.plot_axis.set_title('{}/{}'.format(self.i, self.iterations))
