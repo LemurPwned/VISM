0e9a09b0d613caea1f7588b7e3ce3c0a708e01fe
LemurPwned <jakub.mojsiejuk@gmail.com>
2018-03-29 13:00:03 +0200

    adding Windows force build

--- buildVerifier.py
+++ buildVerifier.py
@@ -6,17 +6,25 @@ class BuildVerifier:
     def __init__(self):
         self.__OSTYPE__ = self.os_deocde(platform.system())
         self.cython_builds()
-        
+
     def os_deocde(self, os_string):
         if os_string == 'Darwin':
             return 'Macintosh'
         else:
             return os_string
 
+
+	# python3 cython_modules/ex_setup.py build_ext --build-lib cython_modules/build --inplace
+
     def cython_builds(self):
         # enforce cython build
         if self.__OSTYPE__ == "Windows":
-            print("Please follow instructions on webpage to build cython manually")
+            result = subprocess.run(['python', 'cython_modules/ex_setup.py',
+                                    'build_ext', '--build-lib',
+                                    'cython_modules/build',
+                                    '--inplace'], stdout=subprocess.PIPE)
+            print(result.stdout.decode('utf-8'))
+            print("If above build failed, please follow instructions on webpage to build cython manually")
         else:
             # build from makefile
             result = subprocess.run(['make', 'cython'], stdout=subprocess.PIPE)
