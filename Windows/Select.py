73f7342477c763a8820ebc33c74eb293e83b65b7
LemurPwned <jakub.mojsiejuk@gmail.com>
2018-05-13 08:48:34 +0200

    adding new files

---
+++ Windows/Select.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QComboBox, QGroupBox, \
+                QVBoxLayout, QRadioButton, QLabel, QSlider, QPushButton, QMessageBox
+from Windows.SelectTemplate import Ui_Dialog
+from PopUp import PopUpWrapper
+from Widgets.openGL_widgets.AbstractGLContext import AbstractGLContext
+
+class Select(QWidget, Ui_Dialog):
+    def __init__(self):
+        super(Select, self).__init__()
+        self.setWindowTitle("Select text to display")
+        self.setupUi(self)
+        self.show()
+
+    def accept(self):
+        if AbstractGLContext.ANY_GL_WIDGET_IN_VIEW:
+            AbstractGLContext.TEXT = self.lineEdit.text()
+            # enable recording
+            AbstractGLContext.RECORD_REGION_SELECTION = True
+            self.hide()
+        else:
+            x = PopUpWrapper(
+                title='No OpenGL widget',
+                msg='Please select OpenGL widget first',
+                more='Not changed',
+                yesMes=None)
+            self.close()
+            
+    def reject(self):
+        self.eventHandler(None)
+        self.close()
---
+++ Windows/SelectTemplate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Windows/UI_files/Select.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(386, 196)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(290, 20, 81, 241))
+        self.buttonBox.setOrientation(QtCore.Qt.Vertical)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 10, 231, 111))
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 130, 251, 41))
+        self.lineEdit.setObjectName("lineEdit")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Enter text and then click ok. After clicking OK, you will be prompted to select text placement "))
+        self.lineEdit.setText(_translate("Dialog", "Enter text"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
